39575bad3585dab550e71714de9f323bae9e6d8c
tbdevnet <tbdevnet@0ba3a8e8-f30d-0410-9ac9-feb71648d401>
2005-06-28 11:41:20 +0000

    *** empty log message ***

---
+++ trunk/WinBits2/test.php
@@ -0,0 +1,123 @@
+<?
+
+function pager($rpp, $count, $href, $opts = array()) {
+    $pages = ceil($count / $rpp);
+
+    if (!isset($opts["lastpagedefault"]))
+        $pagedefault = 0;
+    else {
+        $pagedefault = floor(($count - 1) / $rpp);
+        if ($pagedefault < 0)
+            $pagedefault = 0;
+    }
+
+    if (isset($_GET["page"])) {
+        $page = 0 + $_GET["page"];
+        if ($page < 0)
+            $page = $pagedefault;
+    }
+    else
+        $page = $pagedefault;
+
+    $pager = "";
+
+    $mp = $pages - 1;
+    $as = "<a href=\"{$href}page=0\"><b>&lt;&lt;&nbsp;First&nbsp;&nbsp;</b></a><b>&lt;&lt;&nbsp;Prev</b>";
+    if ($page >= 1) {
+        $pager .= "<a href=\"{$href}page=" . ($page - 1) . "\">";
+        $pager .= $as;
+        $pager .= "</a>";
+    }
+    else
+        $pager .= $as;
+    $pager .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    $as = "<b>Next&nbsp;&gt;&gt;</b>";
+    if ($page < $mp && $mp >= 0) {
+        $pager .= "<a href=\"{$href}page=" . ($page + 1) . "\">";
+        $pager .= $as;
+        $pager .= "</a>";
+    }
+    else
+        $pager .= $as;
+
+    if ($count) {
+        $pagerarr = array();
+        $dotted = 0;
+        $dotspace = 3;
+        $dotend = $pages - $dotspace;
+        $curdotend = $page - $dotspace;
+        $curdotstart = $page + $dotspace;
+        for ($i = 0; $i < $pages; $i++) {
+            if (($i >= $dotspace && $i <= $curdotend) || ($i >= $curdotstart && $i < $dotend)) {
+                if (!$dotted)
+                    $pagerarr[] = "...";
+                $dotted = 1;
+                continue;
+            }
+            $dotted = 0;
+            $start = $i * $rpp + 1;
+            $end = $start + $rpp - 1;
+            if ($end > $count)
+                $end = $count;
+            $text = "$start&nbsp;-&nbsp;$end";
+            if ($i != $page)
+                $pagerarr[] = "<a href=\"{$href}page=$i\"><b>$text</b></a>";
+            else
+                $pagerarr[] = "<b>$text</b>";
+        }
+        $pagerstr = join(" | ", $pagerarr);
+        $pagertop = "<table align=\"center\" border=\"1\"><tr><td>$pager</td></tr><tr><td>$pagerstr</td></tr></table>\n";
+        $pagerbottom = "<table align=\"center\" border=\"1\"><tr><td>$pagerstr</td></tr><tr><td>$pager</td></tr></table>\n";
+    }
+    else {
+        $pagertop = "<p align=\"center\">$pager</p>\n";
+        $pagerbottom = $pagertop;
+    }
+
+    $start = $page * $rpp;
+    $pages = $pages - 1;
+
+    return array($pagertop, $pagerbottom, $page, $pages, "LIMIT $start,$rpp");
+}
+$rpp = 10;
+$count = 200;
+$href = $_SERVER['PHP_SELF']."?";
+
+list($pagertop, $pagerbottom, $page, $pages, $limit) = pager($rpp, $count, $href, $opts = array());
+echo "Now viewing page {$page} of {$pages}<br>";
+echo $pagertop;
+echo "<br>" . $pagerbottom;
+echo $limit;
+
+function getip() {
+if (getenv('HTTP_CLIENT_IP')) {
+			$ip = getenv('HTTP_CLIENT_IP');
+		}
+		elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+			$ip = getenv('HTTP_X_FORWARDED_FOR');
+		}
+		elseif (getenv('HTTP_X_FORWARDED')) {
+			$ip = getenv('HTTP_X_FORWARDED');
+		}
+		elseif (getenv('HTTP_FORWARDED_FOR')) {
+			$ip = getenv('HTTP_FORWARDED_FOR');
+		}
+		elseif (getenv('HTTP_FORWARDED')) {
+			$ip = getenv('HTTP_FORWARDED');
+		}
+		else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+	}
+	
+$realip = getip();
+echo $realip;
+
+$info = apache_lookup_uri('index.php?var=value');
+print_r($info);
+
+if (file_exists($info->filename)) {
+   echo 'file exists!';
+}
+?>
