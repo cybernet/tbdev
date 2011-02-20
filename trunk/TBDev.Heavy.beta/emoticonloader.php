bf9aeb8af8b6ba6276ffc18f5067e8967f42612f
tbdevnet <tbdevnet@0ba3a8e8-f30d-0410-9ac9-feb71648d401>
2011-02-20 17:02:41 +0000

    20th feb 2011 minor update for bug fixes, typos and added short torrentname function

--- trunk/TBDev.Heavy.beta/emoticonloader.php
+++ trunk/TBDev.Heavy.beta/emoticonloader.php
@@ -1,43 +1,54 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+require_once "include/bittorrent.php";
+require_once "include/emoticons.php";
+
+dbconn();
+
+loggedinorreturn();
+
+$htmlout ="
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
+		\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
 		
 		<html xmlns='http://www.w3.org/1999/xhtml'>
 
 		<head>
 
 			<meta name='generator' content='TBDev.net' />
-			<meta http-equiv='Content-Language' content='en-us' />
-			<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+			<meta http-equiv='Content-Language' content='{$TBDEV['language']}' />
+			<meta http-equiv='Content-Type' content='text/html; charset={$TBDEV['char_set']}' />
 			
 			<title>Emoticons Extra</title>
-			<link rel='stylesheet' href='1.css' type='text/css' />
+			<link rel='stylesheet' href='{$TBDEV['stylesheet']}' type='text/css' />
 		</head>
     
     <body>
-    <script type="text/javascript">
+    <script type='text/javascript'>
 <!--
 	function add_smilie(code)
 	{
-		opener.document.bbcode2text.body.value += ' ' + code + ' ';
+		opener.document.getElementById('bbcode2text').body.value += ' ' + code + ' ';
 		//return true;
 	}
 //-->
 </script>
 
-    <table>
+    <table>";
     
-<?php
-require_once "include/emoticons.php";   
+
 
     foreach ($smilies as $k => $v ) {
     
-    print "<tr>
+    $htmlout .= "<tr>
 	  <td align='center' class='row1' valign='middle'><a href=\"javascript:add_smilie('$k')\">$k</a></td>
 	  <td align='center' class='row2' valign='middle'><a href=\"javascript:add_smilie('$k')\"><img src='{$TBDEV['pic_base_url']}smilies/$v' border='0' style='vertical-align:middle;' alt='$v' title='$v' /></a></td>
    </tr>";
     }
 
-?>
+$htmlout .="
     </table>
     </body>
-    </html>
+    </html>";
+    
+ print $htmlout;
+?>
